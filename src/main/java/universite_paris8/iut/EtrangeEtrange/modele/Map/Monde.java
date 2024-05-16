199b44712530b06785332201dd54bc16af141eac
Thomas <thomasgomes2005.94@gmail.com>
2024-05-16 17:34:08 +0200

    Début développement des items au sol : -Liste d'item présent au sol -Item visualisable au sol
    Ajout d'un sprite de drop au sol de l'arc

--- src/main/java/universite_paris8/iut/EtrangeEtrange/modele/Map/Monde.java
+++ src/main/java/universite_paris8/iut/EtrangeEtrange/modele/Map/Monde.java
@@ -4,8 +4,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import universite_paris8.iut.EtrangeEtrange.modele.Entite.Entite;
 import universite_paris8.iut.EtrangeEtrange.modele.Entite.Personnage.Joueur;
+import universite_paris8.iut.EtrangeEtrange.modele.Stockage.DropAuSol;
 import universite_paris8.iut.EtrangeEtrange.modele.Utilitaire.Position;
 import universite_paris8.iut.EtrangeEtrange.modele.Utilitaire.Sommet;
+import universite_paris8.iut.EtrangeEtrange.vues.DropAuSol.gestionAffichageDropAuSol;
 import universite_paris8.iut.EtrangeEtrange.vues.Sprite.gestionAffichageSprite;
 
 import java.io.BufferedReader;
@@ -33,6 +35,7 @@ public class Monde {
      */
 
     private ObservableList<Entite> entites;
+    private ObservableList<DropAuSol> dropsAuSol;
 
     /**
      * Liste des identifiants des éléments du structureMonde :
@@ -41,6 +44,7 @@ public class Monde {
     public Monde(){
         this.sol = new int[sizeMondeHauteur][sizeMondeLargeur];
         this.entites = FXCollections.observableArrayList();
+        this.dropsAuSol = FXCollections.observableArrayList();
     }
 
     /**
@@ -53,6 +57,7 @@ public class Monde {
         this.sol = new int[hauteur][largeur];
         this.traversable = new int[hauteur][largeur];
         this.nontraversable = new int[hauteur][largeur];
+        this.dropsAuSol = FXCollections.observableArrayList();
 
         ArrayList<int[][]> coucheMap = new ArrayList<>();
         coucheMap.add(this.sol);
@@ -100,6 +105,7 @@ public class Monde {
     public Monde(String nom)
     {
         this.entites = FXCollections.observableArrayList();
+        this.dropsAuSol = FXCollections.observableArrayList();
         try
         {
             BufferedReader reader = new BufferedReader(new FileReader(nom));
@@ -179,6 +185,12 @@ public class Monde {
     public ObservableList<Entite> getObservableListEntites(){
         return this.entites;
     }
+    public void ajouterDropAuSol(DropAuSol dropAuSol){
+        this.dropsAuSol.add(dropAuSol);
+    }
+    public void supprimerDropAuSol(DropAuSol dropAuSol){
+        this.dropsAuSol.remove(dropAuSol);
+    }
 
 
     private double calculerDistance(Position pos1, Position pos2)
@@ -253,52 +265,7 @@ public class Monde {
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public void setListenerListeDropsAuSol(gestionAffichageDropAuSol gestionAffichageDropAuSol) {
+        this.dropsAuSol.addListener(gestionAffichageDropAuSol);
+    }
 }

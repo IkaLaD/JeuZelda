02ad47b882d94a58af162d24f2f4eee573f78975
Gafarixo <mbenyoub@iut.univ-paris8.fr>
2024-05-23 19:15:38 +0200

    Implementation competence

--- src/main/java/universite_paris8/iut/EtrangeEtrange/modele/Entite/Personnage/Joueur.java
+++ src/main/java/universite_paris8/iut/EtrangeEtrange/modele/Entite/Personnage/Joueur.java
@@ -21,8 +21,10 @@ import universite_paris8.iut.EtrangeEtrange.modele.Utilitaire.Direction;
 public abstract class Joueur extends Humanoide
 {
     protected Carquois carquois;
+    protected boolean peuCourir;
     public Joueur(double pv, double attaque, double defense, double attaqueSpecial, double defenseSpecial, double vitesse,Sac sac, Objet objetMainGauche, Objet objetMainDroite, Monde monde, double x, double y, Direction direction, Hitbox hitbox) {
         super(pv, attaque, defense, attaqueSpecial, defenseSpecial,vitesse, sac, objetMainGauche, objetMainDroite, monde, x, y, direction, hitbox);
+        peuCourir = false;
     }
 
 
@@ -79,6 +81,12 @@ public abstract class Joueur extends Humanoide
         arme.attaque(actionAttaquer);
 
 
+    }
+
+
+    public void peuCourir(boolean peuCourir)
+    {
+        this.peuCourir = peuCourir;
     }
 
     @Override

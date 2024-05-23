77e3b0dd7ce4d41e9445cf444ac93b7f1578d1d2
Gafarixo <mbenyoub@iut.univ-paris8.fr>
2024-05-23 19:17:57 +0200

    t

--- src/main/java/universite_paris8/iut/EtrangeEtrange/modele/Entite/Personnage/Joueur.java
+++ src/main/java/universite_paris8/iut/EtrangeEtrange/modele/Entite/Personnage/Joueur.java
@@ -20,6 +20,7 @@ import universite_paris8.iut.EtrangeEtrange.modele.Utilitaire.Direction;
 public abstract class Joueur extends Humanoide
 {
     protected Carquois carquois;
+
     public Joueur(double pv, double attaque, double defense, double attaqueSpecial, double defenseSpecial, double vitesse,Sac sac, Objet objetMainGauche, Objet objetMainDroite, Monde monde, double x, double y, Direction direction, Hitbox hitbox) {
         super(pv, attaque, defense, attaqueSpecial, defenseSpecial,vitesse, sac, objetMainGauche, objetMainDroite, monde, x, y, direction, hitbox);
     }

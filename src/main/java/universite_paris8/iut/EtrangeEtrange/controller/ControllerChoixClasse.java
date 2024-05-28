fd116d09e04231bf5722982a11ba004b35442d77
Moha Ben <momocraft00@gmail.com>
2024-05-28 16:47:35 +0200

    Creation vue menu Choix Classe

---
+++ src/main/java/universite_paris8/iut/EtrangeEtrange/controller/ControllerChoixClasse.java
@@ -0,0 +1,13 @@
+package universite_paris8.iut.EtrangeEtrange.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerChoixClasse implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}

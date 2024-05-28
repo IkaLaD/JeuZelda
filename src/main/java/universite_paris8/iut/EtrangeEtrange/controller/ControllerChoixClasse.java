2543502ee9c1dd723833cef1314f5fb9c8738fcb
Moha Ben <momocraft00@gmail.com>
2024-05-28 16:56:18 +0200

    Creation vue menu Choix Classe

--- src/main/java/universite_paris8/iut/EtrangeEtrange/controller/ControllerChoixClasse.java
+++ src/main/java/universite_paris8/iut/EtrangeEtrange/controller/ControllerChoixClasse.java
@@ -1,11 +1,41 @@
 package universite_paris8.iut.EtrangeEtrange.controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class ControllerChoixClasse implements Initializable {
+
+
+
+
+public class ControllerChoixClasse implements Initializable
+{
+    @FXML
+    private Label nomClasse;
+    @FXML
+    private Label descriptionClasse;
+
+
+    @FXML
+    private ProgressBar statPv;
+    @FXML
+    private ProgressBar statAttaque;
+    @FXML
+    private ProgressBar statDefense;
+    @FXML
+    private ProgressBar statAttaqueSpecial;
+    @FXML
+    private ProgressBar statDefenseSpecial;
+    @FXML
+    private ProgressBar statVitesse;
+
+
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
